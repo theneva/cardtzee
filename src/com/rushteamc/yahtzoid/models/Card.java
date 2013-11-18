74c2057cb8d24ad6d0aa15805cd3e445310adb95
Simen Bekkhus <Simen.Bekkhus@accenture.com>
2013-11-18 20:07:01 +0100

    Implement deck

--- src/com/rushteamc/yahtzoid/models/Card.java
+++ src/com/rushteamc/yahtzoid/models/Card.java
@@ -6,5 +6,20 @@ package com.rushteamc.yahtzoid.models;
  */
 public class Card
 {
-    
+    private CardType type;
+
+    public Card(CardType type)
+    {
+        this.setType(type);
+    }
+
+    public CardType getType()
+    {
+        return type;
+    }
+
+    public void setType(CardType type)
+    {
+        this.type = type;
+    }
 }
