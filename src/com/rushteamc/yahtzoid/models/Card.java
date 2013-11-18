16c205e8e3947fff51944c11a57f82d9aa92b6b9
Simen Bekkhus <Simen.Bekkhus@accenture.com>
2013-11-18 19:46:44 +0100

    Add project + Card

---
+++ src/com/rushteamc/yahtzoid/models/Card.java
@@ -0,0 +1,10 @@
+package com.rushteamc.yahtzoid.models;
+
+/**
+ * Created with IntelliJ IDEA. User: Martin Date: 11/18/13 Time: 7:42 PM To change this
+ * template use File | Settings | File Templates.
+ */
+public class Card
+{
+    
+}
